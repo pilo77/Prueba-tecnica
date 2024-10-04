7ac5511c05ed63a04fd41f88c9e23000f882c71f
CARLOS ANDRES VILLAMIL <125434840+pilo77@users.noreply.github.com>
2024-10-03 20:28:45 -0500

    agregando nav

--- front/clienteweb/src/app/app.routes.ts
+++ front/clienteweb/src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
-
 import { RegistroCargosComponent } from './pages/registro-cargos/registro-cargos.component';
-import { RegistroProductosComponent } from './pages/registro-productos/registro-productos.component';
 import { RegistroUsuariosComponent } from './pages/registro-usuarios/registro-usuarios.component';
+import { RegistroProductosComponent } from './pages/registro-productos/registro-productos.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 import { BuscarProductosComponent } from './pages/buscar-productos/buscar-productos.component';
 
 export const routes: Routes = [
-  { path: '', component: RegistroCargosComponent }, // Ruta principal (Inicio)
+  { path: '', component: ListaProductosComponent }, // Puedes cambiar a la página que prefieras como principal
   { path: 'RegistroCargos', component: RegistroCargosComponent },
   { path: 'RegistroUsuarios', component: RegistroUsuariosComponent },
   { path: 'RegistroProductos', component: RegistroProductosComponent },
